801533e0fcd4bba752536f0c8f9682b510719e5e
Eldher Hernández <eldher02@gmail.com>
2018-11-07 16:11:07 -0500

    Create connection.r

---
+++ connection.r
@@ -0,0 +1,23 @@
+
+library(rJava)
+library(RJDBC)
+
+options(java.parameters = '-Xmx8g')
+# hadoop_jar_dirs <- c('/usr/lib/hadoop/lib',
+#                      '/usr/lib/hadoop',
+#                      '/usr/lib/hive/lib')
+# clpath <- c()
+# for (d in hadoop_jar_dirs) {
+#   clpath <- c(clpath, list.files(d, pattern = 'jar', full.names = TRUE))
+# }
+# .jinit(classpath = clpath)
+# .jaddClassPath(clpath)
+
+hive_jdbc_jar <- 'C:/HiveJars/hive-jdbc-1.2.1000.2.5.5.5-2-standalone.jar'
+hive_driver <- 'org.apache.hive.jdbc.HiveDriver'
+hive_url <- "jdbc:hive2://sdpsvrwm0217.scglobal.ad.scotiacapital.com:8443/;ssl=true;sslTrustStore=C:/HiveJars/gateway.jks;trustStorePassword=Test123?hive.server2.transport.mode=http;hive.server2.thrift.http.path=gateway/default/hive"
+drv <- JDBC(hive_driver, hive_jdbc_jar)
+conn <- dbConnect(drv, hive_url, user="123", password="456")
+show_databases <- dbGetQuery(conn, "show databases")
+
+show_databases
