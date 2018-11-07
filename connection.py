22d1e8fb6e9dbead4c6a0797691d2684b81f95cc
Eldher Hernández <eldher02@gmail.com>
2018-11-07 16:15:38 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,13 +4,13 @@ user = "123"
 pass = "456"
 
 conn = jaydebeapi.connect('org.apache.hive.jdbc.HiveDriver',
-"jdbc:hive2://sdpsvrwm0217.scglobal.ad.scotiacapital.com:8443/;ssl=true;sslTrustStore=C:/HiveJars/gateway.jks;trustStorePassword=Test123?hive.server2.transport.mode=http;hive.server2.thrift.http.path=gateway/default/hive",[user,pass])
+"jdbc:hive2://<SERVER>:<PORT>/;ssl=true;sslTrustStore=<GATEWAY_PATH>;trustStorePassword=Test123?hive.server2.transport.mode=http;hive.server2.thrift.http.path=gateway/default/hive",[user,pass])
 
 
 curs = conn.cursor()
 
 #
-#curs.execute("""select * From crz_bbcv_mis.tech_pa_cumst where created_date = '2018-10-30' LIMIT 100""")
+#curs.execute("""select * From table where created_date = '2018-10-30' LIMIT 100""")
 #for row in curs.fetchall():
 #    print(row[2])
 
