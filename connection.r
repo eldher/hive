b6c78465bab8efc96e5e59b1c59713d833dcac32
Eldher Hernández <eldher02@gmail.com>
2018-11-07 16:13:57 -0500

    Update connection.r

--- connection.r
+++ connection.r
@@ -13,9 +13,12 @@ options(java.parameters = '-Xmx8g')
 # .jinit(classpath = clpath)
 # .jaddClassPath(clpath)
 
+
+
+
 hive_jdbc_jar <- 'C:/HiveJars/hive-jdbc-1.2.1000.2.5.5.5-2-standalone.jar'
 hive_driver <- 'org.apache.hive.jdbc.HiveDriver'
-hive_url <- "jdbc:hive2://sdpsvrwm0217.scglobal.ad.scotiacapital.com:8443/;ssl=true;sslTrustStore=C:/HiveJars/gateway.jks;trustStorePassword=Test123?hive.server2.transport.mode=http;hive.server2.thrift.http.path=gateway/default/hive"
+hive_url <- "jdbc:hive2://<server>:<port>/;ssl=true;sslTrustStore=<gateway_path>;trustStorePassword=<Pass>?hive.server2.transport.mode=http;hive.server2.thrift.http.path=gateway/default/hive"
 drv <- JDBC(hive_driver, hive_jdbc_jar)
 conn <- dbConnect(drv, hive_url, user="123", password="456")
 show_databases <- dbGetQuery(conn, "show databases")
